6e13cf223a73dbfc7fd8581fbc96b542fb5c5a68
carl <carl@54a53c1b-c127-0410-8a5d-d1e3cc871925>
2007-01-25 07:03:42 +0000

    Implemented all but two methods
    git-svn-id: http://svn.youngbloods.org/sbn4r/trunk@3 54a53c1b-c127-0410-8a5d-d1e3cc871925

---
+++ lib/combination.rb
@@ -0,0 +1,75 @@
+# takes an array of arrays and iterates over all combinations of subelements.
+# For example:
+# 
+# c = Combination.new([[1, 2], [6, 7, 8]])
+# c.each {|comb| p comb }
+#
+# Will produce:
+#
+# [1, 6]
+# [1, 7]
+# [1, 8]
+# [2, 6]
+# [2, 7]
+# [2, 8]
+
+class Combination
+  include Enumerable
+  
+  def initialize(arr)
+    @arr = arr
+    @current = Array.new(arr.size, 0)
+  end
+  
+  def each
+    iterations = @arr.inject(1) {|product, element| product * element.size } - 1
+    yield current
+    iterations.times { yield self.next_combination }
+  end
+  
+  def <=>(other)
+    @current <=> other.current
+  end
+
+  def first
+    @current.fill 0
+  end
+  
+  def last
+    @current.size.times {|i| @current[i] = @arr[i].size - 1 }
+  end
+  
+  def current
+    returnval = []
+    @current.size.times {|i| returnval[i] = @arr[i][@current[i]] }
+    returnval
+  end 
+  
+  def next_combination
+    i = @current.size - 1
+    @current.reverse.each do |e|
+      if e == @arr[i].size - 1
+        @current[i] = 0
+      else
+        @current[i] += 1
+        break
+      end
+      i -= 1
+    end
+    current
+  end
+  
+  def prev_combination
+    i = @current.size - 1
+    @current.reverse.each do |e|
+      if e == 0
+        @current[i] = @arr[i].size - 1
+      else
+        @current[i] -= 1
+        break
+      end
+      i -= 1
+    end
+    current
+  end
+end
--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,18 @@
 class Sbn
   class Node
-    attr_accessor :name
+    attr_accessor :name, :states
     
-    def initialize(name = '', states = [])
-      @name = name
+    def initialize(name = '', states = [], probabilities = [])
+      @name = name.to_sym
       @children = []
+      @parents = []
       @states = []
-      @probabilities = {}
       set_states(states)
+      set_probabilities(probabilities)
+    end
+    
+    def to_s
+      @name.to_s
     end
     
     def add_child(node)
@@ -23,36 +28,91 @@ class Sbn
     end
     
     def set_states(states)
+      states.symbolize_values!
       @states = states
+      generate_state_table
     end
     
     def set_probability(probability, event)
-      
+      event.symbolize_keys_and_values!
+      c = [event[@name]]
+      index = state_combinations.index(c)
+      @probabilities[index] = probability
+      generate_state_table
     end
     
-  private
-    def can_be_evaluated?(event)
-      
+    def set_probabilities(probs)
+      @probabilities = probs
+      generate_state_table
+    end
+
+  	# A node can't be evaluated unless its parent nodes have
+  	# been observed    
+    def can_be_evaluated?(evidence)
+      returnval = true
+      parents.each {|p| returnval = false unless evidence.has_key?(p.name) }
+      returnval
     end
     
-    def get_random_state(event)
-      
+  	# In order to draw uniformly from the probabilty space, we can't
+  	# just pick a random state.  Instead we generate a random number
+  	# between zero and one and iterate through the states until the 
+  	# cumulative sum of their probabilities exceeds our random number.    
+    def get_random_state(event = {})
+      event.symbolize_keys_and_values!
+      sum = 0.0
+      num = rand
+      returnval = nil
+      @states.each do |s|
+        returnval = s
+        r = evaluate_marginal(s, event)
+        sum += r
+        break if num < sum
+      end
+      returnval      
     end
     
     def get_random_state_with_markov_blanket(event)
       
     end
     
+  private
+    def state_combinations
+      all_states = [@states]
+      @parents.each do |p|
+        c << event[p.name]
+        all_states << p.states
+      end
+      Combination.new(all_states).to_a
+    end
+  
+    def generate_state_table
+      return unless @probabilities
+      probs = @probabilities.dup
+      @state_table = state_combinations.collect {|e| [e, probs.shift] }
+    end
+  
     def remove_irrelevant_states(probabilities, state, evidence)
-      
+      probabilities.reject! {|element| element.first.first != state }
+      index = 1
+      @parents.each do |node|
+        raise "Marginal cannot be evaluated because not all parent nodes are set" unless evidence.has_key?(node.name)
+        probabilities.reject! {|element| element.first[index] != evidence[node.name] }
+        index += 1
+      end
+      probabilities
     end
     
     def evaluate_marginal(state, event)
-      
+      temp_probs = @state_table.dup
+      remove_irrelevant_states(temp_probs, state, event)
+      sum = 0.0
+      temp_probs.each {|e| sum += e[1] }
+      sum
     end
     
     def evaluate_markov_blanket(state, event)
       
-    end    
+    end
   end
 end
