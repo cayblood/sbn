f6ed86058297958e01e51b61c6758d11de85662b
carl <carl@54a53c1b-c127-0410-8a5d-d1e3cc871925>
2007-03-02 19:19:14 +0000

    Consolidating tests
    git-svn-id: http://svn.youngbloods.org/sbn4r/trunk@44 54a53c1b-c127-0410-8a5d-d1e3cc871925

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -2,6 +2,27 @@ require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/sbn4r'
 
 class TestHelpers < Test::Unit::TestCase       # :nodoc: all
+
+  # Tests for Enumerable helpers
+  def test_sum
+    assert_equal 45, (1..9).sum
+  end
+
+  def test_average
+    # Ranges don't have a length
+    assert_in_delta 5.0, (1..9).to_a.average, 0.01
+  end
+
+  def test_sample_variance
+    assert_in_delta 6.6666, (1..9).to_a.sample_variance, 0.0001
+  end
+
+  def test_standard_deviation
+    assert_in_delta 2.5819, (1..9).to_a.standard_deviation, 0.0001
+  end
+
+
+
   def test_ngrams
     assert_equal "THIS IS A STRING".ngrams(2), ["TH", "HI", "IS", "S ", " I", "IS", "S ", " A", "A ", " S", "ST", "TR", "RI", "IN", "NG"]
     assert_equal "THIS IS A STRING".ngrams(3), ["THI", "HIS", "IS ", "S I", " IS", "IS ", "S A", " A ", "A S", " ST", "STR", "TRI", "RIN", "ING"]
