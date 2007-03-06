5263a22c5876cb03a69753b8d09b4ad28dec56a2
Carl Youngblood <carl@youngbloods.org>
2007-03-06 21:53:20 +0000

    Changed main include file to sbn.rb
    git-svn-id: http://svn.youngbloods.org/sbn4r/trunk@54 54a53c1b-c127-0410-8a5d-d1e3cc871925

--- lib/sbn4r.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'active_support'
-gem 'builder', '>=2.0'
-require 'builder'
-
-Dir[File.join(File.dirname(__FILE__), '*.rb')].sort.each { |lib| require lib unless lib == 'sbn4r.rb' }
